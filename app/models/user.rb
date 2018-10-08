12b1b980da867767582b3aac12efeba124e6bdc9
Luiz Henrique <luizhenrique.bernardes@tocanet.com.br>
2018-10-08 15:33:50 -0300

    authorize with no pattern

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def admin?
+    self.admin
+  end
+
+  def moderator?
+    return true;
+  end
+
 end
